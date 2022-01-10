1c9bd29b89c13cc760d8800019195fd9661e30f3
pchekushkin <pchekushkin@clink.tech>
2022-01-10 15:43:33 +0400

    Тестовое задание на Angular. Настройка Unit тестирования с помощью Jest

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      "astTransformers": [
+        "jest-preset-angular/build/InlineFilesTransformer",
+        "jest-preset-angular/build/StripStylesTransformer"
+      ]
+    },
+  },
+  coverageDirectory: 'coverage',
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
